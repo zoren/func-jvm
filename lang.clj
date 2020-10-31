49a3aca401cde5f73fb5821fbb538bbc51839624
Søren Sjørup <soren.sjorup@deondigital.com>
2020-10-31 21:58:34 +0100

    add invocation of static and instance methods

--- lang.clj
+++ lang.clj
@@ -18,11 +18,6 @@
        (sort-by (juxt :kind :name))
        (pprint/print-table [:kind :name :arity :parameter-types :return-type :flags])))
 
-(ppreflect Integer)
-(ppreflect Long)
-
-(Long. 3)
-
 (def constant-types #{String Long})
 
 (defn type-constant [exp]
@@ -34,51 +29,77 @@
     (catch java.lang.ClassNotFoundException e
       (throw (ex-info "class not found" {:class-name class-name :ex e})))))
 
+(defn check-method [class-obj method-name parameter-types]
+  (try
+    (.getMethod class-obj method-name (into-array Class parameter-types))
+    (catch java.lang.NoSuchMethodException e
+      (throw (ex-info "method not found" {:ex e})))))
+
 (defn type-check [exp]
   (if-let [t (type-constant exp)]
     t
-    (case (first exp)
-      :class
-      (do
-        (check-class (second exp))
-        Class)
-      :construct
-      (let [c (check-class (second exp))
-            args (map type-check (drop 2 exp))]
-        (try
-          (.getConstructor c (into-array Class args))
-          c
-          (catch java.lang.NoSuchMethodException e
-            (throw (ex-info "constructor not found" {:exp exp :args args :ex e})))))
+    (let [[kind & args] exp]
+      (case kind
+        :class
+        (do
+          (check-class (first args))
+          Class)
+        :construct
+        (let [c (check-class (first args))
+              args (map type-check (rest args))]
+          (try
+            (.getConstructor c (into-array Class args))
+            c
+            (catch java.lang.NoSuchMethodException e
+              (throw (ex-info "constructor not found" {:exp exp :args args :ex e})))))
+        :invoke-static-method
+        (let [[class-name method-name & args] args
+              method (check-method (check-class class-name) method-name (map type-check args))]
+          (when-not (-> method .getModifiers java.lang.reflect.Modifier/isStatic)
+            (throw (ex-info "method not static" {:exp exp})))
+          (.getReturnType method))
+        :invoke-instance-method
+        (let [[instance method-name & args] args
+              method (check-method (type-check instance) method-name (map type-check args))]
+          (when (-> method .getModifiers java.lang.reflect.Modifier/isStatic)
+            (throw (ex-info "method not instance" {:exp exp})))
+          (.getReturnType method))
 
-      (throw (ex-info "unknown exp type" {:exp exp})))))
+        (throw (ex-info "unknown exp type" {:exp exp}))))))
 
-(defn eval-exp [exp]
+(defn eval-exp [[kind & args :as exp]]
   (if (type-constant exp)
     exp
-    (case (first exp)
+    (case kind
       :class
-      (Class/forName (second exp))
+      (-> args first Class/forName)
+
       :construct
       (.newInstance
        (.getConstructor
-        (Class/forName (second exp))
-        (into-array Class (map type (drop 2 exp))))
-       (into-array Object (drop 2 exp)))
+        (-> args first Class/forName)
+        (->> args rest (map type) (into-array Class)))
+       (into-array Object (rest args)))
+
+      :invoke-static-method
+      (let [[class-name method-name & args] args
+            method (->> args (map type) (into-array Class) (.getMethod (Class/forName class-name) method-name))]
+        (.invoke method nil (into-array Object args)))
+
+      :invoke-instance-method
+      (let [[instance method-name & args] args
+            method (->> args (map type) (into-array Class) (.getMethod (class instance) method-name))]
+        (.invoke method instance (into-array Object args)))
 
       (throw (ex-info "unknown exp type" {:exp exp})))))
 
-(defn comp [exp])
+(let [[kind & args] 3]
+  [kind args])
 
 (comment
-  (ppreflect java.lang.AtomicInteger)
-  (type-check [:class "d"])
-  (reflect/reflect Integer)
-  (ppreflect Integer)
-
   (type-check "abc")
   (type-check 123)
-  (type-check [:class "java.lang.Integer"])
+  (type-check [:class "java.lang.Long"])
   (type-check [:class "NoSuchClass"])
   (type-check [:construct "java.lang.Object"])
   (type-check [:construct "java.lang.Long" "23"])
@@ -86,25 +107,9 @@
   (eval-exp [:construct "java.lang.Long" "23"])
   (type-check [:construct "java.math.BigDecimal" "23"])
   (eval-exp [:construct "java.lang.Long" 34])
-                                        ;  (Object.)
-  (->
-   [:construct "java.lang.Integer" ["123"]]
-   )
+  (type-check [:invoke-static-method "java.lang.Long" "getLong" "java.specification.version" 34])
+  (eval-exp [:invoke-static-method "java.lang.Long" "getLong" "java.specification.version" 34])
 
-  (-> "java.lang.Integer"
-      Class/forName
-      (.getConstructor
-       (into-array Class [String String])
-       )
-      )
-
-  (-> "java.lang.Long"
-      Class/forName
-      (.getConstructor
-       (into-array Class [(Class/forName "java.lang.Long")])
-       )
-      (.newInstance
-       (into-array Object [123])
-       )
-      )
+  (type-check [:invoke-instance-method 12 "toString"])
+  (eval-exp [:invoke-instance-method 12 "toString"])
   )
