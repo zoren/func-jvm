3c220b0e1aab8d06d242f187f8f240c89c7d7222
Søren Sjørup <soren.sjorup@deondigital.com>
2020-11-04 19:45:00 +0100

    refac

--- src/lang.clj
+++ src/lang.clj
@@ -37,58 +37,64 @@
 (defn static? [member]
   (-> member .getModifiers java.lang.reflect.Modifier/isStatic))
 
+(def annotated-type (comp :type meta))
+
+(defn with-type
+  ([exp t] (with-type exp t nil))
+  ([exp t additionals] (with-meta exp (merge {:type t} additionals))))
+
 (defn annotate [[kind & args :as exp]]
   (case kind
     :constant
-    (with-meta exp {:type (-> args first type-constant)})
+    (with-type exp (-> args first type-constant))
     :class
-    (with-meta exp {:type Class :class (check-class (first args))})
+    (with-type exp Class {:class (check-class (first args))})
     :get-static-field
     (let [[class-name field-name] args
           class-obj (check-class class-name)
           field (check-field class-obj field-name)]
       (when-not (static? field)
         (throw (ex-info "field not static" {:exp exp})))
-      (with-meta exp {:type (.getType field) :class class-obj :field field}))
+      (with-type exp (.getType field) {:class class-obj :field field}))
     :get-instance-field
     (let [[instance-exp field-name] args
           annotated-instance (annotate instance-exp)
-          field (check-field (-> annotated-instance meta :type) field-name)]
+          field (check-field (annotated-type annotated-instance) field-name)]
       (when (static? field)
         (throw (ex-info "field not instance" {:exp exp})))
-      (with-meta [:get-instance-field annotated-instance field-name] {:type (.getType field) :field field}))
+      (with-type [kind annotated-instance field-name] (.getType field) {:field field}))
     :construct
     (let [[class-name & args] args
           c (check-class class-name)
           annotated-args (map annotate args)
-          ctor (check-constructor c (map (fn [a] (-> a meta :type)) annotated-args))]
-      (with-meta (into [:construct class-name] annotated-args) {:type c :ctor ctor}))
+          ctor (check-constructor c (map annotated-type annotated-args))]
+      (with-type (into [kind class-name] annotated-args) c {:ctor ctor}))
     :invoke-static-method
     (let [[class-name method-name & args] args
           annotated-args (map annotate args)
-          method (check-method (check-class class-name) method-name (map (fn [a] (-> a meta :type)) annotated-args))]
+          method (check-method (check-class class-name) method-name (map annotated-type annotated-args))]
       (when-not (static? method)
         (throw (ex-info "method not static" {:exp exp})))
-      (with-meta (into [:invoke-static-method class-name method-name] annotated-args)
-        {:type (.getReturnType method) :method method}))
+      (with-type (into [:invoke-static-method class-name method-name] annotated-args)
+        (.getReturnType method) {:method method}))
     :invoke-instance-method
     (let [[instance-exp method-name & args] args
           annotated-instance (annotate instance-exp)
           annotated-args (map annotate args)
-          method (check-method (-> annotated-instance meta :type) method-name (map (fn [a] (-> a meta :type)) annotated-args))]
+          method (check-method (annotated-type annotated-instance) method-name (map annotated-type annotated-args))]
       (when (static? method)
         (throw (ex-info "method not instance" {:exp exp})))
-      (with-meta (into [:invoke-instance-method annotated-instance method-name] annotated-args)
-        {:type (.getReturnType method) :method method}))
+      (with-type (into [:invoke-instance-method annotated-instance method-name] annotated-args)
+        (.getReturnType method) {:method method}))
 
     :if
     (let [[an-cond an-true an-false] (map annotate args)]
-      (when-not (= Boolean (-> an-cond meta :type))
+      (when-not (= Boolean (annotated-type an-cond))
         (throw (ex-info "condition was not boolean" {:exp exp})))
-      (when-not (= (-> an-true meta :type) (-> an-false meta :type))
+      (when-not (= (annotated-type an-true) (annotated-type an-false))
         (throw (ex-info "if branches where different type"
-                        {:exp exp :t-true (-> an-true meta :type) :t-false (-> an-false meta :type)})))
-      (with-meta [:if an-cond an-true an-false] {:type (-> an-true meta :type)}))
+                        {:exp exp :t-true (annotated-type an-true) :t-false (annotated-type an-false)})))
+      (with-type [:if an-cond an-true an-false] (annotated-type an-true)))
 
     (throw (ex-info "unknown exp type" {:exp exp}))))
 
